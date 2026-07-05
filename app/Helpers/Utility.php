efb013c6f47c8cb682f377868c9b7711b4c8f19a
Sadekur <shadekur.rahman60@gmail.com>
2026-07-05 12:56:55 +0600

    Remove unused pri method from Utility class to streamline code

--- app/Helpers/Utility.php
+++ app/Helpers/Utility.php
@@ -52,32 +52,6 @@ class Utility {
 	}
 
 	/**
-	 * Prints information about a variable in a more readable format.
-	 *
-	 * @param mixed $data The variable you want to display.
-	 * @param bool  $admin_only Should it display in wp-admin area only
-	 * @param bool  $hide_adminbar Should it hide the admin bar
-	 */
-	public static function pri( $data, $admin_only = true, $hide_adminbar = true ) {
-
-		if ( $admin_only && ! current_user_can( 'manage_options' ) ) {
-			return;
-		}
-
-		echo '<pre>';
-		if ( is_object( $data ) || is_array( $data ) ) {
-			print_r( $data );
-		} else {
-			var_dump( $data );
-		}
-		echo '</pre>';
-
-		if ( is_admin() && $hide_adminbar ) {
-			echo '<style>#adminmenumain{display:none;}</style>';
-		}
-	}
-
-	/**
 	 * Includes a template file from the 'view' directory.
 	 *
 	 * @param string $template The template file name.
