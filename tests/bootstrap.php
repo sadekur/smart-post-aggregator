3759cbaef5bcee5d3016e9686047cfc70fd5ab76
Sadekur <shadekur.rahman60@gmail.com>
2026-07-05 12:03:02 +0600

    remove unused test files and bootstrap script

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Define WordPress directory and load necessary files
-$wp_dir = '../../../'; // '/var/www/html/wp.wp'
-require_once $wp_dir . '/wp-load.php';
-require_once $wp_dir . '/wp-admin/includes/plugin.php';
-
-// Load the plugin file
-require_once SPA_FILE;
